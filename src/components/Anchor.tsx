7c2dd666aa600ec69c314611bf8a6d000d86f667
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-09-25 19:04:10 -0300

    :white_check_mark: fixes Anchor's props

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -4,25 +4,24 @@ import isObject from '../helpers/isObject';
 
 export interface AnchorProps extends TextProps {
   to?: string | { uri: string; };
-  onPress?: () => void;
 }
 
 /**
  * Anchor component is a textual component which opens `to` prop on press.
  * @param props - Anchor's props.
  */
-const Anchor: React.SFC<AnchorProps> = ({ to, onPress, ...props }) => {
+const Anchor: React.SFC<AnchorProps> = ({ to, children, ...props }) => {
   const url = isObject(to) ? to.uri : to;
 
-  const onAnchorPress = async (): Promise<void> => {
+  const onPress = async (): Promise<void> => {
     const isAllowedToOpen = await Linking.canOpenURL(url);
     if (isAllowedToOpen)
       return Linking.openURL(url);
   };
 
   return (
-    <TouchableOpacity onPress={ onAnchorPress }>
-      <Text { ...props }>{ this.props.children }</Text>
+    <TouchableOpacity onPress={ onPress }>
+      <Text { ...props }>{ children }</Text>
     </TouchableOpacity>
   );
 };
